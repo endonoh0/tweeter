3c81a9f9cb303a935d1fb1fd70e023851e7dc933
Eric Ho <endonoho@gmail.com>
2020-06-16 14:37:40 -0700

    Update, reformat, and refactor css

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(function () {
     $(".new-tweet textarea").on(("keyup"), function () {
         const $tweet = $(this);
         const $tweetLength = $tweet.val().length;
-        const $counter = $tweet.siblings("footer.flex").children("output.counter");
+        const $counter = $tweet.siblings("footer.flexbox").children("output.counter");
 
         $counter.text(140 - $tweetLength);
 
