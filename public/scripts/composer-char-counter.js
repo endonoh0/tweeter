e4301086387b125952cdab8dda127700d6ebf248
Eric Ho <endonoho@gmail.com>
2020-06-16 09:56:10 -0700

    Implement character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $(".new-tweet textarea").on(("keyup"), function () {
+        const $tweet = $(this);
+        const $tweetLength = $tweet.val().length;
+        const $counter = $tweet.siblings("footer.flex").children("output.counter");
+
+        $counter.text(140 - $tweetLength);
+
+        if ($tweetLength > 140) {
+            $counter.addClass("error");
+        } else {
+            $counter.removeClass("error");
+        };
+    });
+});
