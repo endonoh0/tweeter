58169a5dbaf932350d8dff8a849fffd421907e3e
Eric Ho <endonoho@gmail.com>
2020-06-16 22:56:35 -0700

    Implement ajax for post and get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,64 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const createTweetElement = function (tweet) {
+    return $(`
+        <article>
+            <header>
+                <div class="flexbox space-between">
+                    <div class="flexbox">
+                        <img src="${tweet.user.avatars}" alt="profile picture">
+                        <h5>${tweet.user.name}</h5>
+                    </div>
+                        <h4>${tweet.user.handle}</h4>
+                    </div>
+                    <p>${tweet.content.text}</p>
+            <header>
+
+            <hr class="hr-line mb-1">
+
+            <footer class="flexbox space-between">
+                <p class="text-xs">${tweet.created_at}</p>
+                <div>
+                    <button class=""><i class="fa fa-flag"></i></button>
+                    <button class=""><i class="fa fa-retweet"></i></button>
+                    <button class=""><i class="fa fa-heart"></i></button>
+                </div
+            <footer>
+        <article>`
+        );
+}
+
+$(document).ready(function (event) {
+
+    $("form").submit(function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: $(this).attr("action"),
+            method: "POST",
+            data: $(this).serialize(),
+        }).done(function () {;
+            console.log("Success");
+            loadTweets();
+
+        }).fail(function () {
+            alert("Fail");
+        });
+    });
+    loadTweets();
+})
+
+const renderTweets = function (tweets) {
+    const $container = $('#tweets-container').empty();
+
+    tweets.forEach(function (tweet) {
+        $container.append(createTweetElement(tweet));
+    });
+}
+
+const loadTweets = function () {
+    $.ajax("http://localhost:8080/tweets", { method: 'GET' })
+        .done(function (data) {
+            renderTweets(data);
+        });
+}
+
+renderTweets(createTweetElement());
